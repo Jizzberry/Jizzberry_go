e90ee68e97e6c8de1d2e314636e75f48be8c8b1d
Ovenoboyo <ovenoboyo@gmail.com>
2020-06-24 21:51:46 +0530

    Uncomplicate scan
    maybe...
    
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- pkg/tasks_handler/tasks/scan/scan.go
+++ pkg/tasks_handler/tasks/scan/scan.go
@@ -41,7 +41,7 @@ func worker(paths []string, ctx context.Context, progress *int) {
 	}
 	progressMutex := sync.Mutex{}
 	tmp := make(chan int, len(files))
-	*progress = 0
+	updateProgress(progress, 0, len(files), &progressMutex)
 
 	filesModel := files2.Initialize()
 	defer filesModel.Close()
@@ -49,63 +49,31 @@ func worker(paths []string, ctx context.Context, progress *int) {
 	for _, f := range files {
 		maxController <- struct{}{}
 		wg.Add(1)
-		go func(f string, maxController chan struct{}) {
+		go func() {
 			select {
 			case <-ctx.Done():
 				wg.Done()
-				tmp <- 1
-				progressMutex.Lock()
-				*progress = int(float32(len(tmp)) / float32(len(files)) * 100)
-				progressMutex.Unlock()
+				updateProgress(progress, tmp, len(files), &progressMutex)
 				<-maxController
 				return
 			default:
-				file := files2.Files{}
-
-				_, exists := filesModel.IsExists(f)
-
-				if !exists {
-					info, _ := os.Stat(f)
-					ext := filepath.Ext(f)
-					file.FileName = strings.ReplaceAll(info.Name(), ext, "")
-					file.Length = ffmpeg.GetLength(f)
-					file.FileSize = ByteCountDecimal(info.Size())
-					file.DateCreated = time.Unix(info.ModTime().Unix(), 0).Format("01-02-06")
-					file.FilePath = f
-					file.Tags = ""
-					actorsData := tasks_handler.MatchActorToTitle(info.Name())
-					var joinActors string
-					for i, a := range actorsData {
-						scraped := scrapers.ScrapeActor(a)
-						actor_details.Initialize().Create(*scraped)
-						joinString(&joinActors, a.Name, i != len(actorsData)-1)
+				if _, exists := filesModel.IsExists(f); !exists {
+					info, err := os.Stat(f)
+					if err != nil {
+						helpers.LogError(err.Error(), component)
 					}
-					file.Actors = joinActors
-
-					var joinStudios string
-					studiosData := tasks_handler.MatchStudioToTitle(info.Name())
-					for i, s := range studiosData {
-						joinString(&joinStudios, s.Name, i != len(actorsData)-1)
-					}
-
-					file.Studios = joinStudios
-
+					file := createFile(f, info, filepath.Ext(f))
 					genId := filesModel.Create(file)
-
 					ffmpeg.GenerateThumbnail(genId, f, lenInSec(file.Length)/2)
-
 					helpers.LogInfo(fmt.Sprintf("scanned %s successfully", f), component)
 				} else {
 					helpers.LogInfo(fmt.Sprintf("skipped %s", f), component)
 				}
 				wg.Done()
-				tmp <- 1
-				progressMutex.Lock()
-				*progress = int(float32(len(tmp)) / float32(len(files)) * 100)
-				progressMutex.Unlock()
+				updateProgress(progress, tmp, len(files), &progressMutex)
 				<-maxController
 			}
-		}(f, maxController)
+		}()
 	}
 	wg.Wait()
 	close(tmp)
@@ -153,4 +121,44 @@ func joinString(full *string, part string, seperator bool) {
 	if seperator {
 		*full += ", "
 	}
+}
+
+func updateProgress(progress *int, current chan int, total int, mutex *sync.Mutex) {
+	mutex.Lock()
+	current <- 1
+	*progress = int(float32(len(current)/total) * 100)
+	mutex.Unlock()
+}
+
+func getActors(title string) (joinActors string) {
+	actorsData := tasks_handler.MatchActorToTitle(title)
+	for i, a := range actorsData {
+		scraped := scrapers.ScrapeActor(a)
+		actor_details.Initialize().Create(*scraped)
+		joinString(&joinActors, a.Name, i != len(actorsData)-1)
+	}
+	return joinActors
+}
+
+func getStudios(title string) (joinStudios string) {
+	studiosData := tasks_handler.MatchStudioToTitle(title)
+	for i, s := range studiosData {
+		joinString(&joinStudios, s.Name, i != len(studiosData)-1)
+	}
+	return joinStudios
+}
+
+func createFile(filepath string, info os.FileInfo, ext string) files2.Files {
+	file := files2.Files{}
+
+	file.FileName = strings.ReplaceAll(info.Name(), ext, "")
+	file.Length = ffmpeg.GetLength(filepath)
+	file.FileSize = ByteCountDecimal(info.Size())
+	file.DateCreated = time.Unix(info.ModTime().Unix(), 0).Format("01-02-06")
+	file.FilePath = filepath
+	file.Tags = ""
+	file.Actors = getActors(info.Name())
+	file.Studios = getStudios(info.Name())
+
+	return file
 }
